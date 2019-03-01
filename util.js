3fcefeac0cc7da47a274cc09f4dbf2ae3ef65e20
Marijn Haverbeke <marijn@haverbeke.nl>
2019-03-01 13:37:06 +0100

    Upgrade deps

--- util.js
+++ util.js
@@ -1,7 +1,7 @@
 var fs = require("fs");
 
 exports.copyFileSync = function(input, output) {
-  var buff = new Buffer(65536), pos = 0;
+  var buff = Buffer.alloc(65536), pos = 0;
   var infd = fs.openSync(input, "r"), outfd = fs.openSync(output, "w");
   do {
     var read = fs.readSync(infd, buff, 0, 65536, pos);
